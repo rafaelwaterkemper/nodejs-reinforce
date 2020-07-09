275cee2d2ed335fa3cf996ab2274b2a9eaf4b204
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-07-09 00:19:16 -0300

    Created config and connection to use sequelize

--- file-system-typescript/src/persistence/Repository.ts
+++ file-system-typescript/src/persistence/Repository.ts
@@ -1,6 +1,7 @@
 import IEntity from './IEntity';
 
 export default interface Repository {
+    isConnected(): Promise<boolean>;
     save<T extends IEntity>(entity: T): void,
     find(id: Number): IEntity,
     findAll<T extends IEntity>(): Array<T>,
--- file-system-typescript/src/persistence/impl/PostgresRepository.ts
+++ file-system-typescript/src/persistence/impl/PostgresRepository.ts
@@ -1,10 +1,29 @@
 import Repository from './../Repository';
 import IEntity from './../IEntity'
+import { Sequelize } from 'sequelize'
 import { Cachorro, Raca } from './../../model/Cachorro'
 
 export default class PostgresRepository implements Repository {
 
+
+    private _sequelize: Sequelize
+
     constructor() {
+        this._sequelize = this.connect();
+    }
+
+    connect(): Sequelize {
+        return new Sequelize('postgres://waterkemper:waterkemper@localhost:5432/heroes')
+    }
+
+    async isConnected(): Promise<boolean> {
+        try {
+            await this._sequelize.authenticate();
+            return true;
+        } catch (error) {
+            console.error('fail!', error);
+            return false;
+        }
     }
 
     save<T extends IEntity>(entity: T): void {
