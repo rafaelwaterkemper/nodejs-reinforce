2b03f81863e260b7a69d124a191f2e4c6e2caf1b
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-09-02 22:36:31 -0300

    Implement filter, offset and limit at endpoint GET HEROES

--- hapijs/src/routes/HeroeRoute.js
+++ hapijs/src/routes/HeroeRoute.js
@@ -11,7 +11,25 @@ class HeroeRoute extends BaseRoute {
             path: '/api/heroes',
             method: 'GET',
             handler: (req, header) => {
-                return this.db.read()
+                const { nome, skip, limit } = req.query
+
+                try {
+                    if (NaN(skip))
+                        throw Error('Skip should be a number')
+
+                    if (NaN(limit))
+                        throw Error('Limit should be a number')
+
+                    let query = {}
+
+                    if (nome)
+                        query.nome = nome
+
+                    return this.db.read(query, skip, limit)
+                } catch (error) {
+                    console.log('Errowww: ', error)
+                    return 'Erro interno no servidor'
+                }
             }
         }
     }
