d180687330013c2b4e421365962db50101a71567
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2020-09-02 22:54:32 -0300

    Adding Joi to validate request fields

--- hapijs/src/routes/HeroeRoute.js
+++ hapijs/src/routes/HeroeRoute.js
@@ -1,4 +1,5 @@
 const BaseRoute = require('./BaseRoute')
+const Joi = require('joi')
 
 class HeroeRoute extends BaseRoute {
     constructor(db) {
@@ -10,29 +11,42 @@ class HeroeRoute extends BaseRoute {
         return {
             path: '/api/heroes',
             method: 'GET',
+            config: {
+                validate: {
+                    failAction: (req, header, err) => {
+                        throw err
+                    },
+                    //payload
+                    //headers 
+                    //params -> DA url :ID
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(20),
+                        nome: Joi.string().min(3).max(100)
+                    }
+                }
+            },
             handler: (req, header) => {
                 const { nome, skip, limit } = req.query
 
                 try {
-                    if (NaN(skip))
-                        throw Error('Skip should be a number')
-
-                    if (NaN(limit))
-                        throw Error('Limit should be a number')
-
                     let query = {}
 
                     if (nome)
-                        query.nome = nome
+                        query = {
+                            nome: {
+                                $regex: `.*${nome}*.`
+                            }
+                        }
 
                     return this.db.read(query, skip, limit)
-                } catch (error) {
-                    console.log('Errowww: ', error)
-                    return 'Erro interno no servidor'
+                        } catch (error) {
+                            console.log('Errowww: ', error)
+                            return 'Erro interno no servidor'
+                        }
                 }
-            }
+        }
         }
     }
-}
 
 module.exports = HeroeRoute;
